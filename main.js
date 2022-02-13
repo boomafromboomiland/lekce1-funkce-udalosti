7f92dbd5377b8f84517710a29217022ad476645d
boomafromboomiland <lubomira.kovacova@avon.com>
2022-02-13 10:04:52 +0100

    Vypracovanie

--- main.js
+++ main.js
@@ -13,9 +13,31 @@ function secti(a, b) {
   return c;
 }
 
+function spocitaj(a, b) {
+  let c = document.querySelector('#vysledek');
+}
+
+
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
 }
+
+// VYPRACOVANÝ ÚKOL
+
+function priKliknuti() {
+  let ctverecek = document.querySelector('body > div.ctverecek');
+  ctverecek.style.backgroundColor = 'green';
+}
+
+function upozorni() {
+	alert('Gratulace, právě si spustila tuto funkci!');
+	console.log('Gratulace, právě si spustila tuto funkci!');
+  let squareElement = document.getElementsByClassName('ctverecek')[0]
+	squareElement.innerHTML = 'Gratulace!';
+}
+
+
+// ospravedlňujem sa, ale ten zápis pre spočítanie mi moc nedáva zmysel a neviem na to prísť
